53db903675e2f2bc60f5a25851a6f8dd57c1ba6d
Linh <linh.nguyenkhanhib@gmail.com>
2022-11-08 23:05:51 +0200

    changing from fetch API to Axios API

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
-import React, {useEffect, useState} from 'react'
-import Quote from "./Quote"
-
+import React, { useEffect, useState, useCallback } from "react";
+import { ImQuotesLeft, ImQuotesRight } from "react-icons/im";
+import Axios from "axios";
 function App() {
-  const [quote, setQuote] = useState("")
-  const [authors, setAuthors] = useState("")
-  // 'https://api.themotivate365.com/stoic-quote'
+  const [loading, setLoading] = useState(false);
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const url = "https://api.themotivate365.com/stoic-quote";
+
+  const fetchQuoteHandler = useCallback(() => {
+    Axios.get(url)
+      .then((response) => {
+        console.log(response);
+        setQuote(response.data.quote);
+        setAuthor(response.data.author);
+      })
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }, []);
 
   //useEffect(callback, deps)
-  useEffect(() =>{
-    fetch('https://api.themotivate365.com/stoic-quote')
-    .then((res) => res.json())
-    .then(quote =>{
-      setQuote(quote)
-      setAuthors(authors)
-    });
-  },[authors]);
+  useEffect(() => {
+    fetchQuoteHandler();
+  }, [fetchQuoteHandler]);
 
-  //make button to fetch
-  const fetchNewQuote = () =>{
-    fetch('https://api.themotivate365.com/stoic-quote')
-    .then((res) => res.json())
-    .then(quote =>{
-      setQuote(quote);
-  });
+  if (loading) {
+    return <h2>Loading ...</h2>;
   }
 
   return (
     <div className="App">
-      <Quote quote={quote}/>
-      <button onClick={fetchNewQuote}>Generate New Quote</button>
+      <div className="quote-container">
+        <div className="stoic-container" title="stoic">
+          <h2>Stoicism Quote Generator</h2>
+        </div>
+
+        <ImQuotesLeft />
+        <blockquote>{quote}</blockquote>
+        <ImQuotesRight />
+        <div className="authors">{author}</div>
+      </div>
+      <button onClick={fetchQuoteHandler}>Generate New Quote</button>
     </div>
   );
 }
